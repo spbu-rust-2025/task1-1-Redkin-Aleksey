fdc41488464bfedcf598a679a012eeb3f519382f
Redkin-Aleksey <redkin0aleksey@gmail.com>
2025-11-09 20:28:04 +0300

    feat: add homework code to src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::io;
+
 fn main() {
 
+    let mut input = String::new();
+    if let Err(e) = io::stdin().read_line(&mut input) {
+        eprintln!("incorrect input: {}", e);
+        return;
+    }
+
+    let numbers: Vec<&str> = input.trim().split_whitespace().collect();
+
+    if numbers.len() != 2 {
+        eprintln!("it is not 2 numbers");
+        return;
+    }
+
+    let a: i64 = match numbers[0].parse() {
+        Ok(num) => num,
+        Err(e) => {
+            eprintln!("error parsing first number: {}", e);
+            return;
+        }
+    };
+
+    let b: i64 = match numbers[1].parse() {
+        Ok(num) => num,
+        Err(e) => {
+            eprintln!("error parsing second number: {}", e);
+            return;
+        }
+    };
+
+    println!("{}", a + b);
 }
+
